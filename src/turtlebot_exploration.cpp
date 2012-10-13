979f06a54fdfc67afd218ab2eb4c02f62f3b04c5
Tom <tpepels@gmail.com>
2012-10-13 22:14:59 +0300

    Update src/turtlebot_exploration.cpp

--- src/turtlebot_exploration.cpp
+++ src/turtlebot_exploration.cpp
@@ -56,7 +56,7 @@ public:
 
 	float getDistance(float x1, float x2, float y1, float y2)
 	{
-		return sqrt(pow((x1-x2), 2.) + pow((y1 - y2), 2.);
+		return sqrt(pow((x1-x2), 2.) + pow((y1 - y2), 2.));
 	}
 
 	void mapCallback( const nav_msgs::OccupancyGrid& map )
